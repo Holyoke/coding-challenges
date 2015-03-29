380076c55a79711a6b75c15ceab67a4592fb7cf5
Holyoke <kid.idioteque@gmail.com>
2015-03-29 16:51:11 -0700

    Finish sample testing

--- reverse_words/spec/main_spec.rb
+++ reverse_words/spec/main_spec.rb
@@ -53,23 +53,29 @@ describe "#reverse_words" do
 		end
 	end
 
-	context "Sample Cases Files" do
+	context "Sample Input" do
 		let(:sample_input) { File.read("spec/sample_input.txt") }
 		let(:sample_output) { File.read("spec/sample_output.txt")}
 
-		describe "Sample Input" do
+		describe "Sample Input Tests" do
 			it "passes the first sample test" do 
 				input = sample_input.split("\n")[0]
-				output = sample_output("\n")[0]
+				output = sample_output.split("\n")[0]
 
 				reverse_sentence(input)
 
 				expect(input).to eq(output)
-
 			end
 
 			it "passes the second sample test" do 
+				input = sample_input.split("\n")[1]
+				output = sample_output.split("\n")[1]
+
+				reverse_sentence(input)
+
+				expect(input).to eq(output)
 			end
+
 		end
 	end
 end
