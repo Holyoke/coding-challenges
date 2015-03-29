7dda88d4d9684911936df8526d6dcb69cebee296
Holyoke <kid.idioteque@gmail.com>
2015-03-29 15:35:29 -0700

    Begin Reverse Words

--- reverse_words/lib/main.rb
+++ reverse_words/lib/main.rb
@@ -1,45 +1,6 @@
 require "byebug"
 require "set"
 
-def self_describing_numbers_old(num)
-	character_count = character_count(num)
-	position_count = position_count(num)
-
-	state = character_count == position_count.select {|k,v| v > 0}
-
-	state ? "1" : "0"
+def reverse_words(str)
 end
 
-def character_count(str)
-	count = Hash.new(0)
-
-	str.each_char do |c|
-		count[c] += 1
-	end
-
-	count
-end
-
-def position_count(str)
-	number_count = Hash.new(0)
-
-	0.upto( str.length - 1).each do |i|
-		number_count[i.to_s] = str[i].to_i
-	end
-
-	number_count
-end
-
-def self_describing_numbers(str)
-	(0...str.length).each do |i|
-		val = str[i]
-		return "0" unless ( str.count(i.to_s) == val.to_i )
-	end
-	"1"
-end
-
-# str = "6210001000"
-
-# p step_by_step(str)
-# File.open(ARGV[0]).each_line do |line|
-# end
--- reverse_words/spec/main_spec.rb
+++ reverse_words/spec/main_spec.rb
@@ -2,7 +2,7 @@ require "rspec"
 require "main.rb"
 require "byebug"
 
-describe "#self_describing_numbers" do
+describe "#reverse_words" do
 
 	context "Test Case 1 Analysis" do
 	end
@@ -10,33 +10,5 @@ describe "#self_describing_numbers" do
 	context "Sample Cases Files" do
 		let(:sample_input) { File.read("spec/sample_input.txt") }
 		let(:sample_output) { File.read("spec/sample_output.txt")}
-
-		it "passes sample test 1 " do
-
-			input = sample_input.split("\n")[0]
-			answer = sample_output.split("\n")[0]
-
-			output = self_describing_numbers(input)
-
-			expect(output).to  eq(answer)
-		end
-
-		it "passes sample test 2" do
-			input = sample_input.split("\n")[1]
-			answer = sample_output.split("\n")[1]
-
-			output = self_describing_numbers(input)
-
-			expect(output).to  eq(answer)
-		end
-
-		it "passes sample test 3" do
-			input = sample_input.split("\n")[2]
-			answer = sample_output.split("\n")[2]
-
-			output = self_describing_numbers(input)
-
-			expect(output).to  eq(answer)
-		end
 	end
 end
