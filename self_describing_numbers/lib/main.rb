38d0c7dc44c6d09ce4a3b8c21b5bab24d0c743aa
Holyoke <kid.idioteque@gmail.com>
2015-03-29 13:00:16 -0700

    Change test for sample tests

--- self_describing_numbers/lib/main.rb
+++ self_describing_numbers/lib/main.rb
@@ -1,24 +1,33 @@
 require "byebug"
 require "set"
 
-def self_describing_numbers(input)
-	# input = input.split("\n")
+def self_describing_numbers(num)
+	character_count = character_count(num)
+	position_count = position_count(num)
 
+	state = character_count == position_count.select {|k,v| v > 0}
 
+	state ? "1" : "0"
 end
 
 def character_count(str)
+	count = Hash.new(0)
 
+	str.each_char do |c|
+		count[c] += 1
+	end
+
+	count
 end
 
 def position_count(str)
 	number_count = Hash.new(0)
 
-	0.upto( input.length - 1).each do |i|
-		number_count[i] = input[i].to_i
+	0.upto( str.length - 1).each do |i|
+		number_count[i.to_s] = str[i].to_i
 	end
 
 	number_count
 end
-
+# 
 p self_describing_numbers("2020")
--- self_describing_numbers/spec/main_spec.rb
+++ self_describing_numbers/spec/main_spec.rb
@@ -1,30 +1,50 @@
 require "rspec"
 require "main.rb"
+require "byebug"
 
 describe "#self_describing_numbers" do
 
-	context "Test case 1 analysis" do
-
-		it "counts the positions and numbers" do
-			answer = {0=>2, 1=>0, 2=>2, 3=>0}
-			input = "2020"
-			position_count(input).expect == answer
-		end
+	context "Test Case 1 Analysis" do
+		let(:input) { "2020" }
 
 		it "character counts" do
-			input = "2020"
-			answer = { "0" => 2, "1" => 0, "2" => 2, "3" => 0 }
+			answer = { "2" => 2, "0" => 2}
+			result = character_count(input)
 
-			character_count(input).expect == answer
+			expect(result).to  eq(answer)
 		end
+
+		it "analyzes the positions and numbers" do
+			answer = {"0"=>2, "1"=>0, "2"=>2, "3"=>0}
+			result = position_count(input)
+			
+			expect(result).to  eq(answer)
+		end
+
+		it "satisfies self describing conditions" do
+			result = self_describing_numbers(input)
+
+			expect(result).to eq("1")
+		end
+
 	end
 
 
+	context "Sample Cases Files" do
+		let(:sample_input) { File.read("spec/sample_input.txt") }
+		let(:sample_output) { File.read("spec/sample_output.txt")}
 
-	it "passes sample" do
-		input = File.read("spec/sample_input.txt")
-		output = File.read("spec/sample_output.txt")
+		it "passes sample test 1 " do
 
-		self_describing_numbers(output).expect == output.split("\n")
+			input = sample_input.split("\n")[0]
+			output = self_describing_numbers(output)
+			answer = sample_output.split("\n")[0]
+
+			expect(output).to  eq(answer)
+		end
+
+		it "passes sample test 2"
+
+		it "passes sample test 3"
 	end
 end
