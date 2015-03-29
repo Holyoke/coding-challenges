7b9c11c4908b9a829883c8871e65dddd6eb0a03e
Holyoke <kid.idioteque@gmail.com>
2015-03-29 15:39:17 -0700

    Make test cases for in-place solution

--- reverse_words/lib/main.rb
+++ reverse_words/lib/main.rb
@@ -4,3 +4,5 @@ require "set"
 def reverse_words(str)
 end
 
+def swap(str, i, j)
+end
--- reverse_words/spec/main_spec.rb
+++ reverse_words/spec/main_spec.rb
@@ -7,9 +7,17 @@ describe "#reverse_words" do
 	context "Test Case 1 Analysis" do
 		let(:input) { "One Two Three"}
 
+		it "swaps characters around" do
+			str = "abc"
+			answer = "cba"
+
+			result = swap(str,0,2)
+
+			expect(result).to eq(answer)
+		end
+
 		it "reverses a word"
 
-		it "swaps out characters"
 
 		it "reverses sentance"
 	end
