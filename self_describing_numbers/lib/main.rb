8b56396ee06cfac27f054ac553589b0160025b4e
Holyoke <kid.idioteque@gmail.com>
2015-03-29 12:14:40 -0700

    Add smaller tests to self describe

--- self_describing_numbers/lib/main.rb
+++ self_describing_numbers/lib/main.rb
@@ -1,6 +1,24 @@
 require "byebug"
 require "set"
+
 def self_describing_numbers(input)
-	input.split("\n")
+	# input = input.split("\n")
+
+
 end
 
+def character_count(str)
+
+end
+
+def position_count(str)
+	number_count = Hash.new(0)
+
+	0.upto( input.length - 1).each do |i|
+		number_count[i] = input[i].to_i
+	end
+
+	number_count
+end
+
+p self_describing_numbers("2020")
--- self_describing_numbers/spec/main_spec.rb
+++ self_describing_numbers/spec/main_spec.rb
@@ -3,10 +3,28 @@ require "main.rb"
 
 describe "#self_describing_numbers" do
 
+	context "Test case 1 analysis" do
+
+		it "counts the positions and numbers" do
+			answer = {0=>2, 1=>0, 2=>2, 3=>0}
+			input = "2020"
+			position_count(input).expect == answer
+		end
+
+		it "character counts" do
+			input = "2020"
+			answer = { "0" => 2, "1" => 0, "2" => 2, "3" => 0 }
+
+			character_count(input).expect == answer
+		end
+	end
+
+
+
 	it "passes sample" do
 		input = File.read("spec/sample_input.txt")
 		output = File.read("spec/sample_output.txt")
 
-		self_describing_numbers(output).should == output.split("\n")
+		self_describing_numbers(output).expect == output.split("\n")
 	end
 end
