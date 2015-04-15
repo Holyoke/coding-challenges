aa20cbe50c8bf0a69c8d95c0a9a74f1c1630dec5
Holyoke <kid.idioteque@gmail.com>
2015-04-14 22:41:24 -0700

    Finish Productify

--- productify_array/ruby/lib/main.rb
+++ productify_array/ruby/lib/main.rb
@@ -1,7 +1,30 @@
 require "byebug"
 require "set"
 
-def productify(input)
+def productify(arr)
+	result = []
+
+	#populate result with before products
+	product = 1
+	(arr.size).times do |x|
+		result << product if x == 0
+		next if x == arr.size - 1
+
+		product *= arr[x]
+		result << product
+	end
+	
+	#go back and multiply backwards
+	product = 1
+	(arr.size - 1).downto(0) do |x|
+		result[x] *= product
+		product *= arr[x]
+	end
+
+	result
+end
+
+def productify_v1(input)
 	result = []
 
 	input.each do |el|
@@ -9,5 +32,32 @@ def productify(input)
 		products.delete(el)
 		result << products.inject(:*)
 	end
+	result
+end
+
+def productify_before(arr)
+	result = []
+
+	product = 1
+	(arr.size).times do |x|
+		result << product if x == 0
+		next if x == arr.size - 1
+
+		product *= arr[x]
+		result << product
+	end
+	result
+end
+
+def productify_after(arr)
+	result = Array.new(arr.size, 1)
+
+	product = 1
+
+	(arr.size - 1).downto(0) do |x|
+		result[x] = product
+		product *= arr[x]
+	end
+
 	result
 end
--- productify_array/ruby/spec/main_spec.rb
+++ productify_array/ruby/spec/main_spec.rb
@@ -5,6 +5,19 @@ require "byebug"
 describe "#Productify" do
 
 	context "Analysis" do
+		it "finds the product of the integers before" do 
+			input = [3, 1, 2, 5, 6, 4]
+			answer = [1, 3, 3*1, 3*1*2, 3*1*2*5, 3*1*2*5*6]
+
+			expect(productify_before(input)).to eq(answer)
+		end
+
+		it "finds the product of the integers after" do 
+			input = [3, 1, 2, 5, 6, 4]
+			answer = [ 1*2*5*6*4, 2*5*6*4 , 5*6*4 , 6*4 , 4, 1]
+
+			expect(productify_after(input)).to eq(answer)
+		end
 		
 	end
 
@@ -12,6 +25,13 @@ describe "#Productify" do
 		it "Productify's an array" do
 			input = [1, 7, 3, 4]
 			answer = [84, 12, 28, 21]
+
+			expect(productify(input)).to eq(answer)
+		end
+
+		it "Productify's an array with zero's" do
+			input = [1, 7, 0, 4]
+			answer = [0, 0, 28, 0]
 
 			expect(productify(input)).to eq(answer)
 		end
