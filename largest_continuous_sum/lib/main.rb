f33c93c00eeadc6b72d2883f96971177fbe51e3a
Holyoke <kid.idioteque@gmail.com>
2015-04-16 12:37:34 -0700

    Kadane wit #max abstraction

--- largest_continuous_sum/lib/main.rb
+++ largest_continuous_sum/lib/main.rb
@@ -1,6 +1,21 @@
 require "byebug"
 require "set"
 
+def kadane(arr)
+	return if arr.empty?
+
+	max_sum = current_sum = arr.first
+
+	(arr.size).times do |x| 
+		num = arr[x]
+		current_sum += num
+
+		current_sum = [current_sum, num].max
+		max_sum = [current_sum, max_sum].max
+	end
+
+	max_sum
+end
 
 def largest_continuous_sum(arr)
 	return if arr.empty?
@@ -20,20 +35,28 @@ end
 def prakash_largest_continuous_sum(arr)
 	return if arr.empty?
 
-	#set to 
+	#set max, current to first element
 	max_sum = current_sum = arr.first
 
+	# positions to beginning as well
 	start = tstart = back = 0
 
+	# iterate through each index
 	(1...arr.size).each do |pos|
 
+		#if current element is greater than current_sum + current_el
 		if(arr[pos] > current_sum + arr[pos])
+			#set start to current pos
+			# reset current sum
 			tstart = pos
 			current_sum = arr[pos]
 		else
+			#add current_sum
 			current_sum += arr[pos]
 		end
 
+		#check if current_sum is greater than max recorded
+		#if so update everything
 		if (current_sum > max_sum)
 			max_sum = current_sum
 			start = tstart
--- largest_continuous_sum/spec/main_spec.rb
+++ largest_continuous_sum/spec/main_spec.rb
@@ -5,7 +5,12 @@ require "byebug"
 describe "#method" do
 
 	context "Analysis" do
-		it "should find the largest continuous sum" 
+		it "should find the largest continuous sum" do
+			arr = [ -40,1,40,-50,1,50,-20,1,20,0,0]
+			answer = 52
+
+			expect(kadane(arr)).to eq(answer)
+		end
 
 		it "should find the largest continuous sum with indexes"
 	end
