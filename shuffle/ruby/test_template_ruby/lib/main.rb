052eb7c0110b24e427f91cc92daf324de7a4fce8
Holyoke <kid.idioteque@gmail.com>
2015-07-28 19:43:50 -0700

    Attempt shuffle class

--- shuffle/ruby/test_template_ruby/lib/main.rb
+++ shuffle/ruby/test_template_ruby/lib/main.rb
@@ -15,5 +15,43 @@ def myShuffle(arr)
   result
 end
 
-def faroShuffle(arr)
+
+
+class card_Shuffle
+  def initialize(list)
+    @list = list
+  end
+
+  private
+  def riffleShuffle
+    #split in half
+    #random choose the last of the decks to add to the resulting pile
+
+    left = @list.take(@list.size/2)
+    right = @list.drop(@list.size/2)
+    result = []
+
+    until (left.empty? && right.empty?)
+      rng = rand(2) # either 0 or 1
+
+      if (rng == 0 )
+        result.push(left.shift) unless left.empty?
+        result.push(right.shift) unless right.empty?
+      else
+        result.push(right.shift) unless right.empty?
+        result.push(left.shift) unless left.empty?
+      end
+    end
+
+    result
+  end
+
+  def cut
+    idx = rand(@list.size)
+
+    top = @list[@list.size, @list.size - 1]
+    bottom = @list[0, @list.size]
+
+    top + bottom
+  end
 end
